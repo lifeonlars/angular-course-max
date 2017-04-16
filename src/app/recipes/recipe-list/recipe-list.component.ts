1018fd8cbee0c1785416528855ba89887425c03b
Lars Farstad <lars.farstad@gmail.com>
2017-04-16 16:35:45 +1000

    Section 3, lecture 54

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Fat Head Keto Pizza', 'Low carb pizza dough using Mozarella, Cream Cheese, almond flour and egg', 'https://cdn4.ruled.me/wp-content/uploads/2014/08/pepperonipizza.jpg'),
+    new Recipe('Scrambled Eggs with Avocado', 'Keto friendly breakfast with scrambled eggs, avocado and dill', 'https://s-media-cache-ak0.pinimg.com/736x/1a/2a/9f/1a2a9f49e07dba5f6333ab1a2f6b7f3d.jpg')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples',6),
+    new Ingredient('Pears',4),
+    new Ingredient('Tomatoes',5),
+    new Ingredient('Broccoli',2),
+    new Ingredient('Cream',1),
+    new Ingredient('Eggs',12)
+  ];
   constructor() { }
 
   ngOnInit() {
